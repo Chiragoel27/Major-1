a523d4ef6b1364fdb9c52c27e765630eddc11ff4
Your New Name <jain@remote.com>
2023-12-01 01:44:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -113,4 +113,4 @@ async def read_user_me(current_user: User = Depends(get_current_active_user)):
 @app.get("/users/me/items")
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
      return [{"item_id": 1, "owner": current_user}]
-#this is the final comment
+
