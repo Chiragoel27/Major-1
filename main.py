344dc8d7dab63621a085b8423812aeea6ae3d640
Your New Name <jain@remote.com>
2023-12-01 07:27:43 +0530

    Adding Untracked files

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from pydantic import BaseModel
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
-
+import secrets
+#SECRET_KEY=secrets.token_hex(32)
 SCERET_KEY = "151d9e98ad3748ae85d00e2535964619e33a7003da07c869214d312858995734"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 1
@@ -14,7 +15,7 @@ db = {
         "username": "tim",
         "full_name": "Tim Ruscica",
         "email": "tim@gmail.com",
-        "hashed_pass": "$2b$12$26QcftVZwe439mv7F18nMuBV/ZemWwN17OtZ0P4kS1ZSXD2.UDwOm",
+        "hashed_pass": "$2b$12$26QcftVZwe439mv7F18nMuBV/ZemWwN17OtZ0P4kS1ZSXD2.UDwOm", #hash from the name
         "disabled": False
     }
 }
